6f529b4b08938f8944a292f3fa8fb58d0da790f5
Nakano Masaki <namachan10777@gmail.com>
2020-05-29 18:58:02 +0900

    echo driver

--- drivers/lambda/lambda.c
+++ drivers/lambda/lambda.c
@@ -1,10 +1,28 @@
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
 
 #define DRIVER_MEJOR 63
 #define DRIVER_NAME "lambda"
 
+char srcbuf[1024];
+size_t len;
+
+struct runtime_info_t {
+	char *srcbuf;
+	char *outbuf;
+	ssize_t out_size;
+};
+
 static int lambda_open(struct inode *inode, struct file *file) {
+	char *srcbuf = kmalloc(sizeof(char) * 1024, GFP_KERNEL);
+	char *outbuf = kmalloc(sizeof(char) * 1024, GFP_KERNEL);
+	struct runtime_info_t *info = kmalloc(sizeof(struct runtime_info_t), GFP_KERNEL);
+	info->srcbuf = srcbuf;
+	info->outbuf = outbuf;
+	info->out_size = 0;
+	file->private_data = (void*)info;
 	return 0;
 }
 
@@ -13,11 +31,27 @@ static int lambda_release(struct inode *inode, struct file *file) {
 }
 
 static ssize_t lambda_write(struct file *file, const char __user *buf, size_t count , loff_t *f_pos) {
-	return 0;
+	size_t i;
+	struct runtime_info_t *info = file->private_data;
+	if (!access_ok(buf, count)) {
+		return 0;
+	}
+	for (i=0; i<count; ++i) {
+		info->srcbuf[i] = buf[i];
+	}
+	return count;
 }
 
 static ssize_t lambda_read(struct file *file, char __user *buf, size_t count, loff_t *f_pos) {
-	return 0;
+	size_t i;
+	struct runtime_info_t *info = file->private_data;
+	if (!access_ok(buf, count)) {
+		return 0;
+	}
+	for (i=0; i<count && *f_pos < info->out_size; ++i) {
+		buf[i] = info->srcbuf[(*f_pos)++];
+	}
+	return info->out_size - *f_pos;
 }
 
 struct file_operations s_lambda_fops = {
@@ -37,8 +71,6 @@ static void lambda_exit(void) {
 	printk("Goodbye lambda\n");
 	unregister_chrdev(DRIVER_MEJOR, DRIVER_NAME);
 }
-
-
 
 module_init(lambda_init);
 module_exit(lambda_exit);
