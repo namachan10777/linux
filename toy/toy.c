07044bb3dac4c1de06b67c6fc99d82e58c9b3345
Nakano Masaki <namachan10777@gmail.com>
2020-08-06 17:50:27 +0900

    fix toy.c

--- toy/toy.c
+++ toy/toy.c
@@ -8,17 +8,15 @@ SYSCALL_DEFINE3(caesar_encrypt, unsigned int, shift, unsigned char __user *, use
 	long copied_count, failed_to_return_count;
 
 	if (!access_ok(userbuf, count)) {
-		return -EFAULT;
+		return EFAULT;
 	}
-	buf = vmalloc(sizeof(unsigned char) * count);
+	buf = kmalloc(sizeof(unsigned char) * count, GFP_KERNEL);
 	copied_count = strncpy_from_user(buf, userbuf, count);
 	if (copied_count < 0) {
 		printk ("[ caesar_encrypt ] COPY ERROR \n");
-		printk ("[ caesar_encrypt ] userbuf : %s\n", buf );
 		kfree(buf);
-		return - EFAULT ;
+		return EFAULT;
 	}
-
 	for (i=0; i<count; ++i) {
 		unsigned char c = buf[i];
 		if (c >= 0x41 && c <= 0x5a) {
@@ -27,6 +25,12 @@ SYSCALL_DEFINE3(caesar_encrypt, unsigned int, shift, unsigned char __user *, use
 		else if (c >= 0x61 && c <= 0x7a) {
 			buf[i] = ((c - 0x61 + shift) % 27) + 0x61;
 		}
+		else if (c == 0) {
+			break;
+		}
+		else if (c > 0x7f) {
+			return EINVAL;
+		}
 	}
 
 	failed_to_return_count = copy_to_user(userbuf, buf, sizeof(unsigned char) * copied_count);
@@ -34,7 +38,7 @@ SYSCALL_DEFINE3(caesar_encrypt, unsigned int, shift, unsigned char __user *, use
 		kfree(buf);
 		printk ("[ caesar_encrypt ] COPY ERROR \n");
 		printk ("[ caesar_encrypt ] bytes failed to return : %ld\n", failed_to_return_count);
-		return - EFAULT;
+		return EFAULT;
 	}
 	return 0;
 }
